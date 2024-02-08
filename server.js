cc42bb60c169d62a54e00277a592a5ab2d21840d
Abhishek KB <abhishekkb1@gmail.com>
2024-02-08 14:37:19 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,33 +1,66 @@
+require('dotenv').config() // and for using env vairibales(api and secret keys )
 const express=require('express')
 const app=express()
 const ejs=require('ejs')
 const path=require('path')
 const expressLayout=require('express-ejs-layouts')
 const PORT=process.env.PORT || 3000
+const mongoose=require('mongoose')
+const session=require('express-session')
+const flash=require('express-flash')
+const MongoDbStroe=require('connect-mongo')(session)
 
+
+//Database Connection
+mongoose.connect('mongodb://localhost:27017/pizza', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Database connected...');
+})
+.catch(err => {
+    console.log('Connection failed:', err);
+});
+const connection=mongoose.connection;
+
+//Session store
+
+let mongostore=new MongoDbStroe({
+    mongooseConnection:connection,
+    collection:'sessions'
+
+})
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store:mongostore,
+    cookie: { maxAge: 1000 * 60 * 60 * 24}   //--24hrs
+}))
+
+
+app.use(flash())
 //Assets
 app.use(express.static('public'))
+app.use(express.json())
 
+//Global middleware
+app.use((req,res,next)=>{
+    res.locals.session=req.session
+    next()
 
+})
 
 //set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
 
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
+require('./routes/web.js')(app)
+
+
 app.listen(PORT, ()=>{
     console.log('Listening on port 3000')
 })
